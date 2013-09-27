2bdcc98b9f30d6d303edc406aea7a7b9b5fb4af9
Aditya Relangi <arelangi@gmail.com>
2013-09-27 00:19:01 -0500

    added the comments table

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,5 +1,4 @@
 
-create table users (firstName varchar(16), lastName varchar(16), title varchar(4), 
-gender char(1), street varchar(256), city varchar(128), state  varchar(16), email varchar(128),
-password varchar(16), md5Hash char(32),shaHash char(40), phone varchar(20), cell varchar(20),
-ssn varchar(20), picture varchar(256), Primary Key(email));
+create table users (firstName varchar(16), lastName varchar(16), title varchar(4), gender char(1), street varchar(256), city varchar(128), state  varchar(16), email varchar(128), password varchar(16), md5Hash char(32),shaHash char(40), phone varchar(20), cell varchar(20),ssn varchar(20), picture varchar(256), Primary Key(email));
+
+create table comments( thread int, commentID int,  comment text, author varchar(16), timeStmp timestamp, Primary Key(thread,commentId));
