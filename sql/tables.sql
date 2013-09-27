75c6fe574b28b41895e120b82f7198a113fb5b34
Aditya Relangi <arelangi@gmail.com>
2013-09-26 23:45:12 -0500

    added sample users

---
+++ sql/tables.sql
@@ -0,0 +1,5 @@
+
+create table users (firstName varchar(16), lastName varchar(16), title varchar(4), 
+gender char(1), street varchar(256), city varchar(128), state  varchar(16), email varchar(128),
+password varchar(16), md5Hash char(32),shaHash char(40), phone varchar(20), cell varchar(20),
+ssn varchar(20), picture varchar(256), Primary Key(email));
