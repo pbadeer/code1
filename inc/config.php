b575503174ff43bb4c91cb4fa915b9a32e01898b
Aditya Relangi <arelangi@gmail.com>
2013-09-28 03:40:30 -0500

    added backend tables

--- inc/config.php
+++ inc/config.php
@@ -1,1 +1,15 @@
-<?php ?>
+<?php 
+
+	
+	define('DB_HOST','localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS','root');
+	define('DB_NAME','codeOne');
+
+	mysql_connect(DB_HOST,DB_USER,DB_PASS);
+
+	mysql_select_db(DB_NAME)
+	 or die( "Unable to select database");
+
+
+?>
