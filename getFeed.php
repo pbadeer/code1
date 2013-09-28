9660516a272b69a51eae0a7524cd0420a67cb4f6
Aditya Relangi <arelangi@gmail.com>
2013-09-28 14:55:22 -0500

    closes #10

--- getFeed.php
+++ getFeed.php
@@ -1,6 +1,7 @@
 <?php 
 
 	require_once('inc/config.php');
+	require_once('functions.php');
 
 	
 	echo json_encode(getCommentsFor($_GET['id']));
@@ -8,9 +9,10 @@
 
 	function getCommentsFor($ideaId){		
 		$allComments = array();
-		$query = "select * from COMMENTS where IDEAID=".$ideaId;
-		$res = mysql_query($query);
+		$query = "select * from COMMENTS where IDEAID=".$ideaId." order by TIMSTMP desc , UPVOTES desc";
+		$res = mysql_query($query) or die(mysql_error());
 		while($row = mysql_fetch_assoc($res)){
+			$row['timeAgo']= time_elapsed_string($row['TIMSTMP']);
 			$allComments[] = $row;
 		}
 		return $allComments;
